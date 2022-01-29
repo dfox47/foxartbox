fa7f5188ce483d0b9ea4e6206ccb26dd1d1cd51a
fox@teamidea.ru <Zn2g47hZqeDvl>
2022-01-29 23:52:49 +0200

    Import original styles.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<Header />
+	<HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components: {
+		Header,
+		HelloWorld
+	}
 }
 </script>
 
 <style lang="scss">
-	@import 'assets/styles/_styles.scss';
+@import 'assets/styles/_styles.scss';
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,97 +1,26 @@
 <template>
-	<div class="header">
+	<div class="topmenu">
 		<div class="wrap">
-			<header class="header_wrap">
-				<div
-					class="header_bars header_mobile"
-					@click="this.showLangAndCurPopup"
-				>
-					<span />
-					<span />
-					<span />
+			<ul class="topmenu_list">
+				<li class="topmenu_list__item"><a class="js-scroll-to js-topmenu-mobile-close js-topmenu-link active" href="/#top">Home</a></li>
+				<li class="topmenu_list__item"><a class="js-scroll-to js-topmenu-mobile-close js-topmenu-link" href="/#projects">Projects</a></li>
+				<li class="topmenu_list__item"><a class="js-scroll-to js-topmenu-mobile-close js-topmenu-link" href="/#process">Process</a></li>
+				<li class="topmenu_list__item"><a class="js-scroll-to js-topmenu-mobile-close js-topmenu-link" href="/#crew">Crew</a></li>
+				<li class="topmenu_list__item"><a class="js-scroll-to js-topmenu-mobile-close js-topmenu-link" href="/#contacts">Contacts</a></li>
+			</ul>
+
+			<div class="topmenu_mobile">
+				<a class="topmenu_mobile__logo js-scroll-to js-topmenu-mobile-close" href="/#top">
+					<img class="" src="assets/i/icons/logo_2.svg" data-img-src="assets/i/icons/logo_2.svg" alt="" />
+					<span>foxartbox</span>
+				</a>
+
+				<div class="topmenu_mobile__toggle js-topmenu-mobile-toggle">
+					<span></span>
+					<span></span>
+					<span></span>
 				</div>
-
-				<router-link
-					class="logo"
-					to="/"
-				>
-					<img
-						class="logo__img"
-						src="../assets/i/icons/logo.svg"
-						alt=""
-					>
-
-					<img
-						class="logo__text"
-						src="../assets/i/icons/logo_text.svg"
-						alt=""
-					>
-				</router-link>
-
-				<div class="header_right">
-					<img
-						src="../assets/i/photo.png"
-						alt="login_icon"
-						class="header_login_icon header_mobile"
-						@click="showMenuPopup"
-					>
-
-					<ul class="header_nav">
-						<li class="header_nav__item">
-							<router-link to="/send">
-								{{ $vuetify.lang.t('$vuetify.menu_button_send') }}
-							</router-link>
-						</li>
-
-						<li class="header_nav__item">
-							<router-link to="/take">
-								{{ $vuetify.lang.t('$vuetify.menu_button_take') }}
-							</router-link>
-						</li>
-
-						<li class="header_nav__item">
-                            <span
-	                            href="javascript:void(0);"
-	                            @click="this.showLangAndCurPopup"
-                            >
-                                {{ $vuetify.lang.t('$vuetify.language_name') }}, {{ getCurSymbol() }}
-                            </span>
-						</li>
-					</ul>
-
-					<app-lang_and_cur_popup />
-
-					<ul class="login_nav">
-						<li
-							class="login_nav__item"
-							v-if="!isUserLoggedIn"
-						>
-							<v-btn
-								color="secondary"
-								small
-								@click="showLoginPopup"
-							>
-								{{ $vuetify.lang.t('$vuetify.login.button') }}
-							</v-btn>
-						</li>
-						<li
-							class="login_nav__item"
-							v-if="isUserLoggedIn"
-						>
-							<v-avatar
-								color="indigo"
-								@click="showMenuPopup"
-							>
-								<v-icon dark>
-									mdi-account-circle
-								</v-icon>
-							</v-avatar>
-						</li>
-					</ul>
-
-					<app-menu_popup />
-				</div>
-			</header>
+			</div>
 		</div>
 	</div>
 </template>
@@ -99,33 +28,7 @@
 
 
 <script>
-export default {
-	computed: {
-		loading () {
-			return this.$store.getters.loading
-		},
-		isUserLoggedIn () {
-			return this.$store.getters.isUserLoggedIn
-		},
-		user () {
-			return this.$store.getters.user
-		}
-	},
-	methods: {
-		showLangAndCurPopup () {
-			this.$store.dispatch('showLangAndCurPopup')
-		},
-		showLoginPopup () {
-			this.$store.dispatch('showLoginPopup')
-		},
-		showMenuPopup () {
-			this.$store.dispatch('showMenuPopup')
-		},
-		getCurSymbol () {
-			return this.$store.getters.getCurSymbol
-		}
-	}
-}
+export default {}
 </script>
 
 
