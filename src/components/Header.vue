b9897eab5a6d532b2ed978c6d71db90c78141aee
Dennis FOX <dfox@foxartbox.com>
2022-03-08 14:17:06 +0200

    Header links with scroll.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,19 @@
 <template>
+	<div id="top"></div>
+
 	<div class="topmenu">
 		<div class="wrap">
 			<ul class="topmenu_list">
-				<li class="topmenu_list__item"><a class="js-scroll-to js-topmenu-mobile-close js-topmenu-link active" href="/#top">Home</a></li>
-				<li class="topmenu_list__item"><a class="js-scroll-to js-topmenu-mobile-close js-topmenu-link" href="/#projects">Projects</a></li>
-				<li class="topmenu_list__item"><a class="js-scroll-to js-topmenu-mobile-close js-topmenu-link" href="/#process">Process</a></li>
-				<li class="topmenu_list__item"><a class="js-scroll-to js-topmenu-mobile-close js-topmenu-link" href="/#crew">Crew</a></li>
-				<li class="topmenu_list__item"><a class="js-scroll-to js-topmenu-mobile-close js-topmenu-link" href="/#contacts">Contacts</a></li>
+				<li class="topmenu_list__item"><a class="js-topmenu-mobile-close js-topmenu-link active" @click="scroll('top')">Home</a></li>
+				<li class="topmenu_list__item"><a class="js-topmenu-mobile-close js-topmenu-link" @click="scroll('projects')">Projects</a></li>
+				<li class="topmenu_list__item"><a class="js-topmenu-mobile-close js-topmenu-link" @click="scroll('process')">Process</a></li>
+				<li class="topmenu_list__item"><a class="js-topmenu-mobile-close js-topmenu-link" @click="scroll('crew')">Crew</a></li>
+				<li class="topmenu_list__item"><a class="js-topmenu-mobile-close js-topmenu-link" @click="scroll('contacts')">Contacts</a></li>
 			</ul>
 
 			<div class="topmenu_mobile">
-				<a class="topmenu_mobile__logo js-scroll-to js-topmenu-mobile-close" href="/#top">
-					<img class="" src="../assets/i/icons/logo_2.svg" alt="" />
+				<a class="topmenu_mobile__logo js-topmenu-mobile-close" @click="scroll('top')">
+					<img class="" src="../assets/i/icons/logo_2.svg" alt=""/>
 					<span>foxartbox</span>
 				</a>
 
@@ -24,3 +26,15 @@
 		</div>
 	</div>
 </template>
+
+<script>
+export default {
+	methods: {
+		scroll(id) {
+			document.getElementById(id).scrollIntoView({
+				behavior: 'smooth'
+			});
+		}
+	}
+}
+</script>
