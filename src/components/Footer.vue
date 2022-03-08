8f2ee66159dcfca20c742b25febba12db131461a
Dennis FOX <dfox@foxartbox.com>
2022-03-08 13:49:30 +0200

    Footer icons links.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,11 +4,11 @@
 			<a class="gotop js-scroll-to" href="/#top"></a>
 
 			<div class="footer__links">
-				<a href="//www.behance.net/alicefox" target="_blank" title="Behance"><img class="" src="/wp-content/uploads/2020/01/icons/behance.svg" data-img-src="/wp-content/uploads/2020/01/icons/behance.svg" alt="Behance"></a>
+				<a href="//www.behance.net/alicefox" target="_blank" title="Behance"><img class="" src="../assets/i/icons/behance.svg" alt="Behance"></a>
 
-				<a href="//web.facebook.com/foxartbox/" target="_blank" title="Facebook"><img class="" src="/wp-content/uploads/2020/01/icons/fb.svg" data-img-src="/wp-content/uploads/2020/01/icons/fb.svg" alt="Facebook"></a>
+				<a href="//web.facebook.com/foxartbox/" target="_blank" title="Facebook"><img class="" src="../assets/i/icons/fb.svg" alt="Facebook"></a>
 
-				<a href="//www.instagram.com/foxartbox/" target="_blank" title="Instagram"><img class="" src="/wp-content/uploads/2020/01/icons/inst.svg" data-img-src="/wp-content/uploads/2020/01/icons/inst.svg" alt="Instagram"></a>
+				<a href="//www.instagram.com/foxartbox/" target="_blank" title="Instagram"><img class="" src="../assets/i/icons/inst.svg" alt="Instagram"></a>
 			</div>
 
 			<p class="footer__desc">Connect with us</p>
