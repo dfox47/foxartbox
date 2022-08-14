b136464b6a5f5e9fef61b23bc09c73ef9d9fd496
dfox <dfox@foxartbox.com>
2022-08-14 17:28:09 +0300

    Footer refactoring

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,9 +4,9 @@
 			<a class="gotop js-scroll-to" href="/#top"></a>
 
 			<div class="social_links">
-				<a class="social_links__item" href="//web.facebook.com/foxartbox/" target="_blank" title="Facebook"><img class="" src="../assets/i/icons/fb.svg" alt="Facebook"></a>
+				<a class="social_links__item" href="//web.facebook.com/foxartbox/" target="_blank" title="Facebook"><img src="../assets/i/icons/fb.svg" alt="Facebook"></a>
 
-				<a class="social_links__item" href="//www.instagram.com/foxartbox/" target="_blank" title="Instagram"><img class="" src="../assets/i/icons/inst.svg" alt="Instagram"></a>
+				<a class="social_links__item" href="//www.instagram.com/foxartbox/" target="_blank" title="Instagram"><img src="../assets/i/icons/inst.svg" alt="Instagram"></a>
 			</div>
 
 <!--			<p class="footer__desc">Connect with us</p>-->
