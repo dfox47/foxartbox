bb0fa93935b6387006e457a7b948de02aff4645c
dfox <dfox@foxartbox.com>
2022-08-14 16:53:18 +0300

    Contacts styles refactoring

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -4,15 +4,15 @@
 
 		<h3>Let’s get in touch</h3>
 
-		<p>Want to discuss your ideas for a new project —<br>or just say hello? It’s all good, we’d love to connect with you.</p>
+		<p>Want to discuss your ideas for a new project —<br />or just say hello? It’s all good, we’d love to connect with you.</p>
 
 		<ul class="contacts_links">
-			<li>
-				<a href="mailto:info@foxartbox.com" target="_blank"><img src="../assets/i/icons/email.svg" alt="" /><span>info@foxartbox.com</span></a>
+			<li class="contacts_links__item">
+				<a class="contacts_links__link" href="mailto:info@foxartbox.com" target="_blank"><img class="contacts_links__img" src="../assets/i/icons/email.svg" alt="" /><span>info@foxartbox.com</span></a>
 			</li>
 
-			<li>
-				<a href="javascript:void(0);"><img src="../assets/i/icons/skype.svg" alt="" /><span>Skype: foxartbox</span></a>
+			<li class="contacts_links__item">
+				<a class="contacts_links__link" href="javascript:void(0);"><img class="contacts_links__img" src="../assets/i/icons/skype.svg" alt="" /><span>Skype: foxartbox</span></a>
 			</li>
 		</ul>
 	</div>
