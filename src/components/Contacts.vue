721603f508dfdd1d882257317724b8234f92144a
dfox <dfox@foxartbox.com>
2022-08-14 18:56:32 +0300

    Contacts refactoring.

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -1,19 +1,27 @@
 <template>
 	<div id="contacts" class="contacts">
-		<h2>Contact us</h2>
+		<h2>Contacts</h2>
 
-		<h3>Let’s get in touch</h3>
+		<div class="contacts__content">
+			<h3>Let’s get in touch</h3>
 
-		<p>Want to discuss your ideas for a new project —<br />or just say hello? It’s all good, we’d love to connect with you.</p>
+			<p>Want to discuss your ideas for a new project — or just say hello?</p>
 
-		<ul class="contacts_links">
-			<li class="contacts_links__item">
-				<a class="contacts_links__link" href="mailto:info@foxartbox.com" target="_blank"><img class="contacts_links__img" src="../assets/i/icons/email.svg" alt="" /><span>info@foxartbox.com</span></a>
-			</li>
+			<p>It’s all good, we’d love to connect with you.</p>
 
-			<li class="contacts_links__item">
-				<a class="contacts_links__link" href="javascript:void(0);"><img class="contacts_links__img" src="../assets/i/icons/skype.svg" alt="" /><span>Skype: foxartbox</span></a>
-			</li>
-		</ul>
+			<ul class="contacts_links">
+				<li class="contacts_links__item">
+					<a class="contacts_links__link" href="mailto:info@foxartbox.com" target="_blank"><img class="contacts_links__img" src="../assets/i/icons/email.svg" alt="" /><span>info@foxartbox.com</span></a>
+				</li>
+
+				<li class="contacts_links__item">
+					<a class="contacts_links__link" href="javascript:void(0);"><img class="contacts_links__img" src="../assets/i/icons/skype.svg" alt="" /><span>foxartbox</span></a>
+				</li>
+
+				<li class="contacts_links__item">
+					<a class="contacts_links__link" href="//t.me/dfoxweb" target="_blank"><img class="contacts_links__img" src="../assets/i/icons/telegram.svg" alt="" /><span>@dfoxweb</span></a>
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
