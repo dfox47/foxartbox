4de4f84e9cc790ba1c3c89127141d8f470eab8b4
dfox <dfox@foxartbox.com>
2022-08-14 15:35:03 +0300

    Social links styles.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,10 +3,8 @@
 		<div class="wrap">
 			<a class="gotop js-scroll-to" href="/#top"></a>
 
-			<div class="footer__links">
-				<a href="//www.behance.net/alicefox" target="_blank" title="Behance"><img class="" src="../assets/i/icons/behance.svg" alt="Behance"></a>
-
-				<a href="//web.facebook.com/foxartbox/" target="_blank" title="Facebook"><img class="" src="../assets/i/icons/fb.svg" alt="Facebook"></a>
+			<div class="social_links">
+				<a class="social_links__item" href="//web.facebook.com/foxartbox/" target="_blank" title="Facebook"><img class="" src="../assets/i/icons/fb.svg" alt="Facebook"></a>
 
 				<a href="//www.instagram.com/foxartbox/" target="_blank" title="Instagram"><img class="" src="../assets/i/icons/inst.svg" alt="Instagram"></a>
 			</div>
