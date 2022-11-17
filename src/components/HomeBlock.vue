4f417b3209f9ba72c7844343e50f9668ed3106a3
dfox <dfox@foxartbox.com>
2022-11-17 12:08:03 +0200

    refactoring home block

--- src/components/HomeBlock.vue
+++ src/components/HomeBlock.vue
@@ -1,12 +1,12 @@
 <template>
-	<div class="home_block js-home-block">
+	<div class="home_block js-home-block" @scroll="onScroll">
 		<canvas id="projector" class="home_block__bg" width="1266" height="941"></canvas>
 
 		<div class="home_block__content js-home-block-content">
 			<img class="logo" src="../assets/i/icons/logo_2.svg" alt="" />
 
-			<div class="home_block__desc js-home-block-desc">foxartbox</div>
-			<div class="home_block__slogan js-random-slogan">fast websites</div>
+			<div class="home_block__desc js-home-block-desc">Foxartbox</div>
+			<div class="home_block__slogan js-random-slogan">Let's make it fast</div>
 			<a class="btn btn__link js-scroll-to" @click="scroll('projects')">Projects</a>
 		</div>
 	</div>
@@ -14,12 +14,16 @@
 
 <script>
 export default {
-	// methods: {
-	// 	scroll(id) {
-	// 		document.getElementById(id).scrollIntoView({
-	// 			behavior: 'smooth'
-	// 		});
-	// 	}
-	// }
+	created () {
+		window.addEventListener('scroll', this.handleScroll);
+	},
+	unmounted () {
+		window.removeEventListener('scroll', this.handleScroll);
+	},
+	methods: {
+		handleScroll () {
+			// Any code to be executed when the window is scrolled
+		}
+	}
 }
 </script>
