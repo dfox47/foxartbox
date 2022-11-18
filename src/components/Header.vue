b2ce7361b7109d3733a9b6ae0dd04c4cd44f14fd
dfox <dfox@foxartbox.com>
2022-11-18 08:54:01 +0200

    Mobile. Hide topmenu after click on menu item. Refactoring

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,12 +6,12 @@
 			<div class="wrap">
 				<ul class="topmenu_list">
 					<li class="topmenu_list__item" v-for="(menuItem, index) in menuItems" :key="menuItem.id">
-						<a :class="{'active': index === 0}" class="js-topmenu-mobile-close js-topmenu-link" @click="scroll(menuItem.id), toggleMobileMenu()" href="javascript:void(0);">{{ menuItem.title }}</a>
+						<a :class="{'active': index === 0}" @click="scroll(menuItem.id); toggleMobileMenu()" href="javascript:void(0);">{{ menuItem.title }}</a>
 					</li>
 				</ul>
 
 				<div class="topmenu_mobile">
-					<a class="topmenu_mobile__logo js-topmenu-mobile-close" @click="scroll('top'), toggleMobileMenu()">
+					<a class="topmenu_mobile__logo" @click="scroll('top'); toggleMobileMenu()">
 						<img src="../assets/i/icons/logo_2.svg" alt="" />
 						<span>foxartbox</span>
 					</a>
