38f58ce8ef0a8d694f897d10eef796c116c069a6
dfox <dfox@foxartbox.com>
2022-12-15 11:44:57 +0200

    Process update

--- src/components/Process.vue
+++ src/components/Process.vue
@@ -1,85 +1,30 @@
 <template>
 	<div>
-		<h2 class="process_title js-block-scroll" id="process">Process</h2>
+		<div class="wrap">
+			<h2 class="js-block-scroll" id="process">Process</h2>
 
-		<div class="wp-block-group">
-			<div class="wp-block-group__inner-container">
-				<div class="wp-block-group process_list">
-					<div class="wp-block-group__inner-container">
-						<div class="wp-block-columns process_list_item">
-							<div class="wp-block-column">
-								<img src="../assets/i/icons/process_1.svg" alt="" />
-							</div>
+			<ol class="process">
+				<li class="process__item" v-for="processItem in processItems" :key="processItem.title">
+					<h3>{{ processItem.title }}</h3>
 
-							<div class="wp-block-column">
-								<h5>Сonnect</h5>
-
-								<p>The project begins with a discussion of your needs and ideas. To start just write us «Hello!»</p>
-							</div>
-						</div>
-
-						<div class="wp-block-columns process_list_item">
-							<div class="wp-block-column">
-								<img src="../assets/i/icons/process_2.svg" alt="" />
-							</div>
-
-							<div class="wp-block-column">
-								<h5>Prototype</h5>
-
-								<p>We collect your data for creating a detailed plan for the future website.</p>
-							</div>
-						</div>
-
-						<div class="wp-block-columns process_list_item">
-							<div class="wp-block-column">
-								<img src="../assets/i/icons/process_3.svg" alt="" />
-							</div>
-
-							<div class="wp-block-column">
-								<h5>Design</h5>
-
-								<p>We do marketing research to offer you the most suitable graphic solution. Your website will be mobile friendly, optimized and responsive.</p>
-							</div>
-						</div>
-
-						<div class="wp-block-columns process_list_item">
-							<div class="wp-block-column">
-								<img src="../assets/i/icons/process_4.svg" alt="" />
-							</div>
-
-							<div class="wp-block-column">
-								<h5>Programming</h5>
-
-								<p>Design comes to life by programming. We work with popular CMS. Therefore future managing of your the project will be easy.</p>
-							</div>
-						</div>
-
-						<div class="wp-block-columns process_list_item">
-							<div class="wp-block-column">
-								<img src="../assets/i/icons/process_5.svg" alt="" />
-							</div>
-
-							<div class="wp-block-column">
-								<h5>Test &amp; publish</h5>
-
-								<p>Before publishing the website online we test it to be sure that everything works properly.</p>
-							</div>
-						</div>
-
-						<div class="wp-block-columns process_list_item">
-							<div class="wp-block-column">
-								<img src="../assets/i/icons/process_6.svg" alt="" />
-							</div>
-
-							<div class="wp-block-column">
-								<h5>Maintaince</h5>
-
-								<p>You can manage your project independently, but if you need help we will happy to provide a support.</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+					<p class="process__desc">{{ processItem.desc }}</p>
+				</li>
+			</ol>
 		</div>
 	</div>
 </template>
+
+<script>
+export default {
+	name: 'Process',
+	data() {
+		return {
+			processItems: [
+				{title: 'Connect',      desc: "The project begins with a discussion of your needs and ideas."},
+				{title: 'Development',  desc: "Design, programming, SEO - that's our part of work"},
+				{title: 'Support',      desc: "You can manage your project independently, but if you need help we will happy to provide a support."}
+			],
+		}
+	},
+}
+</script>
