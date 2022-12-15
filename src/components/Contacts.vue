a68bb6d7330b847abf981e42001f3a156752b9fe
dfox <dfox@foxartbox.com>
2022-12-15 12:04:56 +0200

    Contacts. Add phone

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -5,7 +5,7 @@
 		<div class="contacts__content">
 			<ul class="contacts_links">
 				<li class="contacts_links__item" v-for="(contactsLink) in contactsLinks" :key="contactsLink.href">
-					<a class="contacts_links__link" :href="contactsLink.href" target="_blank"><img class="contacts_links__img" :src="contactsLink.icon" alt="" /><span>{{ contactsLink.title }}</span></a>
+					<a class="contacts_links__link" :href="contactsLink.href" target="_blank"><img class="contacts_links__img" :src="contactsLink.icon" alt=""><span>{{ contactsLink.title }}</span></a>
 				</li>
 			</ul>
 		</div>
@@ -17,8 +17,9 @@ export default {
 	data() {
 		return {
 			contactsLinks: [
-				{href: 'mailto:info@foxartbox.com', icon: require('../assets/i/icons/email.svg'), title: 'info@foxartbox.com'},
-				{href: '//t.me/dfoxweb', icon: require('../assets/i/icons/telegram.svg'), title: '@dfoxweb'}
+				{href: 'mailto:info@foxartbox.com', icon: require('../assets/i/icons/email.svg'),       title: 'info@foxartbox.com'},
+				{href: '//t.me/dfoxweb',            icon: require('../assets/i/icons/telegram.svg'),    title: '@dfoxweb'},
+				{href: 'tel:+359882744430',         icon: '',                                           title: '+359 (88) 27-444-30'}
 			]
 		}
 	}
