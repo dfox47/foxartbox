206844dc4711b2dcc0537942279934a49cd1619b
dfox <dfox@foxartbox.com>
2022-12-20 00:51:35 +0200

    About page

---
+++ src/components/About.vue
@@ -0,0 +1,15 @@
+<template>
+	<div id="about" class="about">
+		<div class="wrap">
+			<h2>About</h2>
+
+			<div>
+				<p>Hi! My name is Denis FOX.</p>
+
+				<p>I'm a frontend developer and CEO of FOXARTBOX.</p>
+
+				<p>My team includes designer, backend developer, copywriter & SEO specialist</p>
+			</div>
+		</div>
+	</div>
+</template>
