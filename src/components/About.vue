c73632e9e543b887432b485640fc7dfb833dbad1
dfox <dfox@foxartbox.com>
2022-12-20 18:25:03 +0200

    About. Commit text

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,14 +1,14 @@
 <template>
 	<div id="about" class="about">
 		<div class="wrap">
-			<h2>About</h2>
+<!--			<h2>About</h2>-->
 
 			<div>
-				<p>Hi! My name is Denis FOX.</p>
+<!--				<p>Hi! My name is Denis FOX.</p>-->
 
-				<p>I'm a frontend developer and CEO of FOXARTBOX.</p>
+<!--				<p>I'm a frontend developer and CEO of FOXARTBOX.</p>-->
 
-				<p>My team includes designer, backend developer, copywriter & SEO specialist</p>
+<!--				<p>My team includes designer, backend developer, copywriter & SEO specialist</p>-->
 			</div>
 		</div>
 	</div>
