14c47c5ccb790739df1ee488ca802d86b8fce569
dfox <dfox@foxartbox.com>
2022-12-25 10:37:56 +0200

    Change logo

--- src/components/HomeBlock.vue
+++ src/components/HomeBlock.vue
@@ -3,7 +3,7 @@
 		<canvas id="projector" class="home_block__bg" width="1266" height="941"></canvas>
 
 		<div class="home_block__content js-home-block-content">
-			<img class="logo" src="../assets/i/icons/logo_2.svg" alt="" />
+			<img class="logo" src="../assets/i/icons/logo_3.svg" alt="" />
 			<div class="home_block__desc js-home-block-desc">Foxartbox</div>
 			<div class="home_block__slogan js-random-slogan"></div>
 			<a class="btn btn__link js-scroll-to" @click="scroll('projects')">Projects</a>
