4fdaff35c887b616d3fb6efebaf3399be9b932e3
dfox <dfox@foxartbox.com>
2022-12-26 09:57:48 +0200

    Blog page to index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import {createRouter, createWebHistory} from 'vue-router'
 
 import Home from '../views/Home'
+import Blog from '../views/Blog'
 
 const routes = [
 	{
 		component: Home,
 		name: 'Home',
 		path: '/'
+	},
+	{
+		component: Blog,
+		name: 'Blog',
+		path: '/blog/'
 	}
 ]
 
