4b7fa6cbd9d9de9897a22e2fe3f32e189eef720a
dfox <dfox@foxartbox.com>
2022-12-26 09:58:10 +0200

    Blog vue

---
+++ src/views/Blog.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="content_inner">
+		<div class="wrap">
+			<h1>Blog</h1>
+
+			<p>Here will be BLOG with JS</p>
+		</div>
+	</div>
+</template>
+
+<script>
+// import About        from '@/components/About'
+
+export default {
+	components: {
+		// About,
+		// Contacts,
+		// HomeBlock,
+		// Process,
+		// Projects
+	},
+	created() {
+		document.title = "Blog | Foxartbox"
+	}
+}
+</script>
