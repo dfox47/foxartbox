8f87bbe9badff9d086a12f36225fa72d0e35ff4f
dfox <dfox@foxartbox.com>
2023-01-03 18:48:19 +0200

    ScrollTo refactoring. Part 4

--- src/assets/js/scrollTo.js
+++ src/assets/js/scrollTo.js
@@ -1,6 +1,6 @@
 // scroll to the block with ID
 const scrollTo = (id) => {
-	const e = document.getElementById(id.replace('/#', ''))
+	const e = document.getElementById(id.replace('/#', '').replace('#', ''))
 
 	// if there is no element with ID to scroll
 	if (!e) {
