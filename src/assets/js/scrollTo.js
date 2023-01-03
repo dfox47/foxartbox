c7a4845a9e2f53351f8a5ead18b4c12cde9f5e34
dfox <dfox@foxartbox.com>
2023-01-03 18:46:19 +0200

    ScrollTo to independent file

---
+++ src/assets/js/scrollTo.js
@@ -0,0 +1,18 @@
+// scroll to the block with ID
+const scrollTo = (id) => {
+	const e = document.getElementById(id.replace('/#', ''))
+
+	// if there is no element with ID to scroll
+	if (!e) {
+		window.location.href = id
+
+		return
+	}
+
+	// scroll to element
+	e.scrollIntoView({
+		behavior: 'smooth'
+	})
+}
+
+export default scrollTo
