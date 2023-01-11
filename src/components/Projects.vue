0a8253ae8f229d4cf895e15513163fabc474c512
dfox <dfox@foxartbox.com>
2023-01-11 18:03:14 +0200

    Projects. Refactoring design. Part 2

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,47 +4,43 @@
 
 		<div class="wrap">
 			<div class="projects_list">
-				<a class="projects_list__link" href="javascript:void(0);" v-for="project in projects" :key="project.name" :style="'background-image: url(' + project.mainImg + ');'">
+				<a
+					:key="project.name"
+					:style="'background-image: url(' + project.mainImg + ');'"
+					@click="popupShow(project.id)"
+					class="projects_list__link"
+					href="javascript:void(0);"
+					v-for="project in projects"
+				>
 					<span class="projects_list__title">{{ project.name }}</span>
-				</a>
-			</div>
-		</div>
 
-		<div class="project" v-for="project in projects" :key="project.name">
-			<div class="project__content" :class="'project_bg_type_' + project.id">
-				<div class="wrap">
-					<img class="project__img" :src="project.mainImg" alt="">
-
-					<div class="project__desc">
+					<div class="hidden js-project-desc" :data-popup="project.id">
 						<div class="project__title">
 							<h3>{{ project.title }}</h3>
 
 							<div class="project__price">{{ project.price }}</div>
 						</div>
 
-						<p>{{ project.desc }}</p>
+						<p class="project__desc">{{ project.desc }}</p>
 
 						<p><a :href="'//' + project.href" target="_blank">www.{{ project.href }}</a></p>
+
+						<div class="project_images">
+							<img class="project_images__item" v-for="img in project.images" :src="img.src" alt="" :key="img.src">
+						</div>
 					</div>
-
-					<p class="text-center">
-						<a class="btn btn__main js-popup-show" @click="popupShow(project.id)" href="javascript:void(0);" :data-popup="project.id"><span>Show more</span></a>
-					</p>
-				</div>
+				</a>
 			</div>
+		</div>
+	</div>
 
-			<!--			<div class="popupX active js-popup" :data-popup-id="'project_id_' + project.id">-->
-			<!--				<div class="popup__bg" @click="popupHide()"></div>-->
+	<div class="popup js-popup">
+		<div class="popup__bg" @click="popupHide()"></div>
 
-			<div class="popup__content_wrapX">
-				<!--					<a class="btn btn__close popup__close" href="javascript:void(0);" @click="popupHide()"></a>-->
+		<div class="popup__content_wrap">
+			<a class="btn btn__close popup__close" href="javascript:void(0);" @click="popupHide()"></a>
 
-				<div class="popup__content">
-					<!-- examples -->
-					<!-- https://ismail9k.github.io/vue3-carousel/examples.html -->
-				</div>
-			</div>
-			<!--			</div>-->
+			<div class="popup__content js-popup-content"></div>
 		</div>
 	</div>
 </template>
@@ -63,10 +59,10 @@ export default {
 					mainImg: require('../assets/i/projects/brokertop/0.jpg'),
 					price: '5000 € | 3 months',
 					title: 'Brokertop | Real estate agency',
-					imgs: [
-						{img: require('../assets/i/projects/brokertop/1.jpg')},
-						{img: require('../assets/i/projects/brokertop/2.jpg')},
-						{img: require('../assets/i/projects/brokertop/3.jpg')}
+					images: [
+						{src: require('../assets/i/projects/brokertop/1.jpg')},
+						{src: require('../assets/i/projects/brokertop/2.jpg')},
+						{src: require('../assets/i/projects/brokertop/3.jpg')}
 					]
 				},
 				{
@@ -77,10 +73,10 @@ export default {
 					mainImg: require('../assets/i/projects/chocolana/0.png'),
 					price: '3500 € | 3 months',
 					title: 'Chocolana online shop',
-					imgs: [
-						{img: require('../assets/i/projects/chocolana/1.jpg')},
-						{img: require('../assets/i/projects/chocolana/2.jpg')},
-						{img: require('../assets/i/projects/chocolana/3.jpg')}
+					images: [
+						{src: require('../assets/i/projects/chocolana/1.jpg')},
+						{src: require('../assets/i/projects/chocolana/2.jpg')},
+						{src: require('../assets/i/projects/chocolana/3.jpg')}
 					]
 				},
 				{
@@ -91,11 +87,11 @@ export default {
 					mainImg: require('../assets/i/projects/villabansko/0.png'),
 					price: '2500 € | 1 month',
 					title: 'Villa Pirin Golf',
-					imgs: [
-						{img: require('../assets/i/projects/villabansko/1.jpg')},
-						{img: require('../assets/i/projects/villabansko/2.jpg')},
-						{img: require('../assets/i/projects/villabansko/3.jpg')},
-						{img: require('../assets/i/projects/villabansko/4.jpg')}
+					images: [
+						{src: require('../assets/i/projects/villabansko/1.jpg')},
+						{src: require('../assets/i/projects/villabansko/2.jpg')},
+						{src: require('../assets/i/projects/villabansko/3.jpg')},
+						{src: require('../assets/i/projects/villabansko/4.jpg')}
 					]
 				},
 				{
@@ -106,14 +102,14 @@ export default {
 					mainImg: require('../assets/i/projects/p3week/0.png'),
 					price: '7800 € | 4 months',
 					title: 'P3week forum',
-					imgs: [
-						{img: require('../assets/i/projects/p3week/1.jpg')},
-						{img: require('../assets/i/projects/p3week/2.jpg')},
-						{img: require('../assets/i/projects/p3week/3.jpg')},
-						{img: require('../assets/i/projects/p3week/4.jpg')},
-						{img: require('../assets/i/projects/p3week/5.jpg')},
-						{img: require('../assets/i/projects/p3week/6.jpg')},
-						{img: require('../assets/i/projects/p3week/7.jpg')}
+					images: [
+						{src: require('../assets/i/projects/p3week/1.jpg')},
+						{src: require('../assets/i/projects/p3week/2.jpg')},
+						{src: require('../assets/i/projects/p3week/3.jpg')},
+						{src: require('../assets/i/projects/p3week/4.jpg')},
+						{src: require('../assets/i/projects/p3week/5.jpg')},
+						{src: require('../assets/i/projects/p3week/6.jpg')},
+						{src: require('../assets/i/projects/p3week/7.jpg')}
 					]
 				}
 			]
@@ -122,24 +118,19 @@ export default {
 	methods: {
 		// hide popup
 		popupHide() {
-			// document.querySelector('html').classList.remove('popup_active')
-
-			document.querySelectorAll('.js-popup').forEach((e) => {
-				e.classList.remove('active')
-			})
+			document.querySelector('html').classList.remove('popup_active')
 		},
 		// show popup and put content from template
 		popupShow(id) {
-			document.querySelector('[data-popup-id="project_id_' + id + '"]').classList.add('active')
+			const $contentData  = document.querySelector('.js-project-desc[data-popup="' + id + '"]')
+			const $popupContent = document.querySelector('.js-popup-content')
 
-			// if (!$popup || !$popupContent || !$contentData) return
+			if (!$popupContent || !$contentData) return
 
-			// $popupContent.innerHTML = $contentData.innerHTML
+			$popupContent.innerHTML = $contentData.innerHTML
 
 			// show popup
-			// document.querySelector('html').classList.add('popup_active')
-
-			// console.log('popupId | ', id)
+			document.querySelector('html').classList.add('popup_active')
 		}
 	}
 }
