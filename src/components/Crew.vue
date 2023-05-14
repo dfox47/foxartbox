c4861c905465b53fab4cf0e91cd3e24926c0ac11
dfox <dfox@foxartbox.com>
2023-05-14 17:24:52 +0300

    Crew ref

--- src/components/Crew.vue
+++ src/components/Crew.vue
@@ -5,39 +5,39 @@
 
 			<div class="team_list">
 				<div class="team_item">
-					<img class="team_item__img" src="../assets/i/crew/2.jpg" alt="">
+					<img class="team_item__img" src="../assets/i/crew/2.jpg" alt="" loading="lazy">
 
 					<div class="team_item__name">Daniil TUNEEV</div>
 
 					<div class="team_item__desc">Full stack developer</div>
 
 					<div class="team_item__skills">
-						<img class="" src="../assets/i/icons/js.svg" alt="js" title="js">
-						<img class="" src="../assets/i/icons/gulp.svg" alt="Gulp" title="Gulp">
-						<img class="" src="../assets/i/icons/python.svg" alt="Python" title="Python">
-						<img class="" src="../assets/i/icons/c++.svg" alt="C++" title="C++">
-						<img class="" src="../assets/i/icons/mysql.svg" alt="MySQL" title="MySQL">
-						<img class="" src="../assets/i/icons/html.svg" alt="html" title="html">
-						<img class="" src="../assets/i/icons/css.svg" alt="CSS" title="CSS">
-						<img class="" src="../assets/i/icons/less.svg" alt="less" title="less">
-						<img class="" src="../assets/i/icons/sass.svg" alt="Sass" title="Sass">
-						<img class="" src="../assets/i/icons/figma.svg" alt="Figma" title="figma">
+						<img class="" src="../assets/i/icons/js.svg" alt="js" title="js" loading="lazy">
+						<img class="" src="../assets/i/icons/gulp.svg" alt="Gulp" title="Gulp" loading="lazy">
+						<img class="" src="../assets/i/icons/python.svg" alt="Python" title="Python" loading="lazy">
+						<img class="" src="../assets/i/icons/c++.svg" alt="C++" title="C++" loading="lazy">
+						<img class="" src="../assets/i/icons/mysql.svg" alt="MySQL" title="MySQL" loading="lazy">
+						<img class="" src="../assets/i/icons/html.svg" alt="html" title="html" loading="lazy">
+						<img class="" src="../assets/i/icons/css.svg" alt="CSS" title="CSS" loading="lazy">
+						<img class="" src="../assets/i/icons/less.svg" alt="less" title="less" loading="lazy">
+						<img class="" src="../assets/i/icons/sass.svg" alt="Sass" title="Sass" loading="lazy">
+						<img class="" src="../assets/i/icons/figma.svg" alt="Figma" title="figma" loading="lazy">
 					</div>
 				</div>
 
 				<div class="team_item">
-					<img class="team_item__img" src="../assets/i/crew/4.jpg" alt="">
+					<img class="team_item__img" src="../assets/i/crew/4.jpg" alt="" loading="lazy">
 
 					<div class="team_item__name">Mary BRYKINA</div>
 
 					<div class="team_item__desc">Backend developer</div>
 
 					<div class="team_item__skills">
-						<img class="" src="../assets/i/icons/python.svg" alt="Python" title="Python">
-						<img class="" src="../assets/i/icons/oracle.svg" alt="Oracle" title="Oracle">
-						<img class="" src="../assets/i/icons/js.svg" alt="js" title="js">
-						<img class="" src="../assets/i/icons/html.svg" alt="html" title="html">
-						<img class="" src="../assets/i/icons/css.svg" alt="CSS" title="CSS">
+						<img class="" src="../assets/i/icons/python.svg" alt="Python" title="Python" loading="lazy">
+						<img class="" src="../assets/i/icons/oracle.svg" alt="Oracle" title="Oracle" loading="lazy">
+						<img class="" src="../assets/i/icons/js.svg" alt="js" title="js" loading="lazy">
+						<img class="" src="../assets/i/icons/html.svg" alt="html" title="html" loading="lazy">
+						<img class="" src="../assets/i/icons/css.svg" alt="CSS" title="CSS" loading="lazy">
 					</div>
 				</div>
 
@@ -49,11 +49,11 @@
 					<div class="team_item__desc">Frontend developer</div>
 
 					<div class="team_item__skills">
-						<img class="" src="../assets/i/icons/html.svg" alt="html" title="html">
-						<img class="" src="../assets/i/icons/css.svg" alt="CSS" title="CSS">
-						<img class="" src="../assets/i/icons/sass.svg" alt="Sass" title="Sass">
-						<img class="" src="../assets/i/icons/jquery.svg" alt="jQuery" title="jQuery">
-						<img class="" src="../assets/i/icons/figma.svg" alt="Figma" title="figma">
+						<img class="" src="../assets/i/icons/html.svg" alt="html" title="html" loading="lazy">
+						<img class="" src="../assets/i/icons/css.svg" alt="CSS" title="CSS" loading="lazy">
+						<img class="" src="../assets/i/icons/sass.svg" alt="Sass" title="Sass" loading="lazy">
+						<img class="" src="../assets/i/icons/jquery.svg" alt="jQuery" title="jQuery" loading="lazy">
+						<img class="" src="../assets/i/icons/figma.svg" alt="Figma" title="figma" loading="lazy">
 					</div>
 				</div>
 
@@ -65,9 +65,9 @@
 					<div class="team_item__desc">SEO-specialist</div>
 
 					<div class="team_item__skills">
-						<img class="" src="../assets/i/icons/netpeak-spider.svg" alt="Netpeak Spider" title="Netpeak Spider">
-						<img class="" src="../assets/i/icons/google-search-console.jpg" alt="Google search console" title="Google search console">
-						<img class="" src="../assets/i/icons/yandex-metrika.jpg" alt="Yandex Metrika" title="Yandex Metrika">
+						<img class="" src="../assets/i/icons/netpeak-spider.svg" alt="Netpeak Spider" title="Netpeak Spider" loading="lazy">
+						<img class="" src="../assets/i/icons/google-search-console.jpg" alt="Google search console" title="Google search console" loading="lazy">
+						<img class="" src="../assets/i/icons/yandex-metrika.jpg" alt="Yandex Metrika" title="Yandex Metrika" loading="lazy">
 					</div>
 				</div>
 			</div>
