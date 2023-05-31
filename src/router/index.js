2d384e01b48505cb4196d1b20f67cbe3e76a5a34
dfox <dfox@foxartbox.com>
2023-05-31 17:56:41 +0300

    JS. Router. Make it sensitive

--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,26 @@ const routes = [
 
 const router = createRouter({
 	history: createWebHistory(process.env.BASE_URL),
-	routes
+	routes,
+	sensitive: false
+})
+
+router.beforeEach((to, from, next) => {
+	const _to = to.fullPath.replace(/\/$/, "");
+	//const _from = from.fullPath.replace(/\/$/, "");
+	//const _current = window.location.pathname.replace(/\/$/, "");
+	const flag = _to !== "" && to.fullPath && to.fullPath.match(/\/$/);
+
+	if (flag) {
+		let _to2 = ""
+
+		for (let i = 1; i < to.fullPath.split(/\//).length - 1; i++) {
+			_to2 += "/" + to.fullPath.split(/\//)[i];
+		}
+
+		next(_to2)
+	}
+	else next()
 })
 
 export default router
