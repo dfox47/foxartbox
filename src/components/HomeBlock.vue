af513a8b9b657ae278e2f397e7524bb182437578
dfox <dfox@foxartbox.com>
2023-06-17 17:44:22 +0300

    Home block. Remove test code

--- src/components/HomeBlock.vue
+++ src/components/HomeBlock.vue
@@ -1,11 +1,5 @@
 <template>
 	<div>
-		<div class="equalality_test">
-			<input class="equalality_test__input js-equality-input" type="text">
-
-			<div class="equalality_test__result js-equality-result">---***---</div>
-		</div>
-
 		<div class="home_block js-home-block">
 			<canvas id="projector" class="home_block__bg" width="1266" height="941"></canvas>
 
