471cfe34d8ff86688cf61095e63528ae2a22fb30
dfox <dfox@foxartbox.com>
2023-08-25 15:39:46 +0300

    Leet. Counter ref

--- src/assets/js/interview/counter.js
+++ src/assets/js/interview/counter.js
@@ -4,15 +4,15 @@
 // This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
 
 const counter = () => {
-	/**
-	 * @param {number} n
-	 * @return {Function} counter
-	 */
-	const createCounter = (n) => {
-		return () => {
-			console.log(n)
+	function createCounter(n) {
+		let count = n
 
-			n++
+		return function() {
+			const currentValue = count
+
+			count++
+
+			return currentValue
 		}
 	}
 
