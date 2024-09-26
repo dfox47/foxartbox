f5fb0b8a37a807248d5ae908040b77032263a420
Denis FOX <dfox@foxartbox.com>
2024-09-26 13:50:23 +0300

    Home to Composition API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,24 @@
 <template>
-	<div class="content">
-		<HomeBlock />
-		<About />
-		<Projects />
-		<PageSpeed />
-		<Contacts />
-		<Clock />
-	</div>
+  <div class="content">
+    <HomeBlock />
+    <About />
+    <Projects />
+    <PageSpeed />
+    <Contacts />
+    <Clock />
+  </div>
 </template>
 
-<script>
-import About        from '@/components/About'
-import Clock        from '@/components/Clock'
-import Contacts     from '@/components/Contacts'
-import HomeBlock    from '@/components/HomeBlock'
-import PageSpeed    from '@/components/PageSpeed'
-import Projects     from '@/components/Projects'
+<script setup>
+import {onMounted}  from 'vue'
+import About        from '../components/About'
+import Clock        from '../components/Clock'
+import Contacts     from '../components/Contacts'
+import HomeBlock    from '../components/HomeBlock'
+import PageSpeed    from '../components/PageSpeed'
+import Projects     from '../components/Projects'
 
-export default {
-	name: 'Home',
-	components: {
-		About,
-		Clock,
-		Contacts,
-		HomeBlock,
-		PageSpeed,
-		Projects
-	},
-	created() {
-		document.title = "Home | Foxartbox"
-	}
-}
+onMounted(() => {
+  document.title = 'Home | Foxartbox'
+})
 </script>
